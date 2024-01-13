1e1ca86c2ad5b4f43500bbae525c3f9db414625b
LehhitAbdel <abdel.lehhit@student.ehb.be>
2024-01-13 14:15:33 +0100

    update posts

--- server.js
+++ server.js
@@ -4,9 +4,33 @@ const Post = require('./models/newsPostModel')
 const app = express()
 
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+mongoose.
+connect('mongodb+srv://admin:admin123@nodeapi.1dzfhcd.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(() => {
+    const PORT = 3000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    console.log('connected to db')
+}).catch((error) => {
+    console.log(error)
+})
+
 
 app.get('/test',(req,res)=> {
     res.send('test')
+})
+
+//posts in DB plaatsen
+app.post('/newspost', async (req,res) => {
+    try {
+        const post = await Post.create(req.body)
+        res.status(200).json(post);
+        
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({message: error.message})
+    }
 })
 
 //posts fetchen
@@ -34,27 +58,26 @@ app.get('/newspost/:id', async(req,res) => {
     }
 })
 
+//posts updaten
+app.put('/newspost/:id', async(req,res) => {
 
-//posts in DB plaatsen
-app.post('/newspost', async (req,res) => {
-    try {
-        const post = await Post.create(req.body)
-        res.status(200).json(post);
-        
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).json({message: error.message})
+try {
+
+    const {id} = req.params;
+    const post = await Post.findByIdAndUpdate(id, req.body);
+    if(!post){
+        return res.status(404).json({message: 'cannot find post with id ${id}'})
     }
+    const updatedPost = await Post.findById(id);
+    res.status(200).json(updatedPost);
+    
+} catch (error) {
+    res.status(500).json({message: error.message})
+}
+
 })
 
 
-mongoose.
-connect('mongodb+srv://admin:admin123@nodeapi.1dzfhcd.mongodb.net/Node-API?retryWrites=true&w=majority')
-.then(() => {
-    const PORT = 3000;
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-    console.log('connected to db')
-}).catch((error) => {
-    console.log(error)
-})
+
+
 
